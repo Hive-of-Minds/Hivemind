e258c1270f1a4a0ff7ffb6176391db4916640b3f
divadnebnahtan <davidbennathan@gmail.com>
2021-08-08 09:10:33 +1000

    Changed config format to config.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const {Client} = require('discord.js');
 const client = new Client();
-
-const {config} = require('dotenv');
-config({path: __dirname + "/.env"});
+const {prefix, token, bot_presence} = require('./config.json')
 
 // Event listener for when the bot has started
 client.on('ready', () => {
@@ -10,18 +8,22 @@ client.on('ready', () => {
 
     // Sets the bot's status and activity
     let presence = client.user.setPresence({
-        activity: {name: 'made in javascript :vomiting:'},
-        status: 'online',
+        activity: {name: bot_presence.activity},
+        status: bot_presence.status,
     });
 });
 
 client.on('message', message => {
+    // Exit if message was sent by a bot, not in a server, or doesn't start with the prefix
+    if (!message.guild) return;
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
 
     // Ping command (code will be cleaned up in future)
-    if (message.content === 'h.ping') {
+    if (message.content === `${prefix}ping`) {
         message.channel.send('Pong!');
     }
 })
 
 // Starts the bot
-let login = client.login(process.env.TOKEN);
+let login = client.login(token);
