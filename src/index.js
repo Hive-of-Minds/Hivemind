8e89b8bff35bd7234b112a2837a1cfe52a13781c
divadnebnahtan <davidbennathan@gmail.com>
2021-08-08 10:00:59 +1000

    Cleaned up commands and added test command

--- src/index.js
+++ src/index.js
@@ -19,15 +19,24 @@ client.on('message', message => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
-    // Ping command (code will be cleaned up in future)
-    if (message.content === `${prefix}ping`) {
-        // Sends original message then edits with ping
+    const args = message.content.slice(prefix.length) // Removes the prefix from the message
+        .trim() // Removes all whitespace (blank characters) from the start/end
+        .split(' '); // Separates message by spaces
+    const command = args.shift().toLowerCase(); // gets the first argument (command name) and also removes it from original args
+
+    if (command === 'ping') {
+        // Sends original message then edits with time since
         message.channel.send(`Pong!`).then(async (msg) => {
             await msg.edit(`🏓 Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${client.ws.ping}ms`);
         });
-    } else if (message.content === `${prefix}count`) {
+    } else if (command === 'count') {
         // Displays number of members in server
         message.channel.send(`Total member count: ${message.guild.memberCount}`);
+    } else if (command === 'test'){
+        // Return if no args
+        if (!args.length) return message.channel.send(`You did not input any arguments, ${message.author}!`);
+        // Reply with command info
+        message.channel.send(`Prefix: ${prefix}\nCommand name: ${command}\nArguments: ${args}`);
     }
 })
 
