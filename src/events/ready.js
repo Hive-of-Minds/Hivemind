91ae0d1187bdc833191724680cb7082ad9a13e7b
TardisSyntax666 <80235844+TardisSyntax666@users.noreply.github.com>
2021-08-09 08:28:50 +1000

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 const Event = require('../structures/event.js');
 //hello
 module.exports = new Event('ready', client => {
-    console.log(`I am now ${client.botPresence.status}, my name is ${client.user.username}#${client.user.discriminator}. My prefix is '${client.prefix}'`);
+    console.log(`I am now ${client.botPresence.status}, my name is ${client.user.username}#${client.user.discriminator}. I am blond, 5 foot 8 and like long walks on the beach. I'll show you my prefix if you show me yours. Na jk you can see mine for free its '${client.prefix}'`);
     client.user.setPresence({
         activity: {name: client.botPresence.activity},
         status: client.botPresence.status,
