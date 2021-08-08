7cadf2410f0da7d833a1683d4ca49ffca24d4918
PaxAustralis <640175@trinity.vic.edu.au>
2021-08-08 11:38:30 +1000

    made help.js send an embed instead of a standard message

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,13 @@
+const {MessageEmbed} = require("discord.js");
 module.exports = {
     name: 'help',
     description: 'Help command',
     execute(message, args) {
-        message.channel.send('Help Menu: \n • `h.count` \n • `h.help` \n • `h.ping`');
+        const embed = new MessageEmbed()
+            .setColor('#F1C40F')
+            .setTitle('Help Menu')
+            .setThumbnail('https://cdn.discordapp.com/attachments/873564554674716765/873719057533251644/HiveMind_pfp.png')
+            .setDescription('These are the current commands: \n • `h.count` \n • `h.help` \n • `h.ping`');
+        message.channel.send(embed);
     }
 }
