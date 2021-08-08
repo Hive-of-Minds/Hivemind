e1f8eb85648a5ca4886f46d423cb8ca91bcad758
divadnebnahtan <davidbennathan@gmail.com>
2021-08-08 13:45:51 +1000

    Fixed help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,16 +1,23 @@
+const Command = require('../structures/command.js');
 const {MessageEmbed} = require("discord.js");
-module.exports = {
+
+module.exports = new Command({
     name: 'help',
     description: 'Help command',
-    execute(message, args) {
+
+    async run(message, args, client) {
         const embed = new MessageEmbed()
             .setColor('#DD8505')
-            .setTitle('Help Menu')
+            .setTitle(':gear: Help Menu :gear:')
             .setAuthor(message.author.username, message.author.avatarURL())
             .setThumbnail('https://cdn.discordapp.com/attachments/873564554674716765/873744094390788126/help_command_book.png')
-            .setDescription('These are the current commands: \n • `h.count` \n • `h.help` \n • `h.ping`')
             .setTimestamp()
-            .setFooter("Hivemind is made by cool people and Pax");
-        message.channel.send(embed);
+            .setFooter("HiveMind is made by cool people");
+
+        client.commands.each((command, name) => {
+            embed.addField(name, command.description, false);
+        });
+
+        message.channel.send({embeds: [embed]});
     }
-}
+});
--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,6 @@ const Event = require('../structures/event.js');
 
 module.exports = new Event('ready', client => {
     console.log(`I am now ${client.botPresence.status}, my name is ${client.user.username}#${client.user.discriminator}`);
-
     client.user.setPresence({
         activity: {name: client.botPresence.activity},
         status: client.botPresence.status,
