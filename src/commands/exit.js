c6fd051eab1f742cd7f01fc5ff95a11ca62daf7a
divadnebnahtan <davidbennathan@gmail.com>
2021-08-09 15:39:30 +1000

    Exit command

---
+++ src/commands/exit.js
@@ -0,0 +1,13 @@
+const Command = require('../structures/command.js');
+
+module.exports = new Command({
+    name: 'exit',
+    description: 'exit command',
+    hidden: true,
+    ownerOnly: true,
+
+    async run(message) {
+        await message.reply('Shutting down...');
+        process.exit(0);
+    }
+});
