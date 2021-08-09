24edce7996674e3a4824b13ab910e2b2edf619cf
Pax <83927027+PaxAustralis@users.noreply.github.com>
2021-08-09 17:54:31 +1000

    re-wrote ping command (note: very bad)

---
+++ src/commands/ping.js
@@ -0,0 +1,14 @@
+const Command = require('../structures/command.js');
+
+module.exports = new Command({
+    name: 'ping',
+    aliases: ['pong', 'pingpong'],
+    description: 'Ping command',
+
+    async run(message, args, client) {
+        message.reply(":ping_pong: __Pong!__").then((message2) => {
+            const ping = message2.createdTimestamp - message.createdTimestamp;
+            message.channel.send(`Latency: ${ping} \n API Latency: ${client.ws.ping}`);
+        });
+    }
+})
