693f44112a75ff593c6e1032ea742d9bcf984444
Pax <83927027+PaxAustralis@users.noreply.github.com>
2021-08-22 12:19:20 +1000

    Commented avatar command

--- src/commands/image/avatar.js
+++ src/commands/image/avatar.js
@@ -9,17 +9,20 @@ module.exports = new Command({
     emoji: '📷',
 
     async run(message, args, client) {
+        //Scenario in which multiple members are mentioned after h.avatar
         if (message.mentions.users.size > 1) {
             message.reply(`Correct usage: \`${client.prefix}${this.name} ${this.arguments || ''}\``);
             return;
         }
         const embed = new MessageEmbed();
-        const user = message.mentions.users.first();
+        const user = message.mentions.users.first(); //Assigns the mentioned member to the constant user
+        //Scenario in which no member was mentioned, meaning that user is assigned an undefined value
         if (user === undefined) {
                 embed.setTitle(message.author.tag)
                 .setImage(message.author.displayAvatarURL())
                 .setColor('RANDOM')
             message.reply({embeds: [embed]});
+        //Scenario in which a member was mentioned
         } else {
             embed.setTitle(user.tag)
                 .setImage(user.displayAvatarURL())
