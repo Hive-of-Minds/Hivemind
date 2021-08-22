032d390f96bbed0d8a37a2e2e0d13a7fe8472521
Pax <83927027+PaxAustralis@users.noreply.github.com>
2021-08-22 13:02:56 +1000

    moved mute from fun to moderation

--- src/commands/moderation/mute.js
+++ src/commands/moderation/mute.js
@@ -5,9 +5,10 @@ module.exports = new Command({
     name: 'mute',
     description: 'mutes a person',
     userPermissions: ['MANAGE_ROLES'],
+
     async run(message, args, client) {
-        let member = message.mentions.members.first();
-        let role = message.guild.roles.cache.find(role => role.name === "Muted");
+        let member = message.mentions.members.first(); //Assigns the first member who was mentioned to the variable member
+        let role = message.guild.roles.cache.find(role => role.name === "Muted"); //Accesses the role with the name 'Muted' and assigns it to the variable role
         member.roles.add(role);
         message.channel.send('the dirty deed has been done');
     }
