bc69fa022ffdbccc77eeef137b89a071d03134a8
divadnebnahtan <davidbennathan@gmail.com>
2021-08-24 10:02:44 +1000

    Help is sorted alphabetically

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -8,14 +8,20 @@ module.exports = new Command({
     emoji: '📙',
 
     async run(message, args, client) {
-        const options = Array.from(client.commands.keys()).filter(command => !command.hidden).map(command => { //Accesses the non-hidden commands and returns the following
-            return {
-                label: command.name,
-                description: command.aliases ? command.aliases.join(', ') : null,
-                value: command.name,
-                emoji: command.emoji
-            };
-        });
+        const options = Array.from(client.commands.keys())
+            .filter(command => !command.hidden)
+            .sort((cmd1, cmd2) => { // Sorts commands alphabetically by name
+                const textA = cmd1.name.toUpperCase();
+                const textB = cmd2.name.toUpperCase();
+                return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+            }).map(command => { //Accesses the non-hidden commands and returns the following
+                return {
+                    label: command.name,
+                    description: command.aliases ? command.aliases.join(', ') : null,
+                    value: command.name,
+                    emoji: command.emoji
+                };
+            });
         const row = new MessageActionRow().addComponents(new MessageSelectMenu() //Creates a menu that has the following
             .setCustomId(`help ${client.prefix} ${message.author.id}`)
             .setPlaceholder('No command selected')
