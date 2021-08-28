d88900fece338ea90120ae95400cfa0c1dfb3fa7
divadnebnahtan <davidbennathan@gmail.com>
2021-08-28 12:27:26 +1000

    Help command includes categories

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -8,33 +8,28 @@ module.exports = new Command({
     emoji: '📙',
 
     async run(message, args, client) {
-        const options = Array.from(client.commands.keys())
-            .filter(command => !command.hidden)
-            .sort((cmd1, cmd2) => { // Sorts commands alphabetically by name
-                const textA = cmd1.name.toUpperCase();
-                const textB = cmd2.name.toUpperCase();
-                return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-            }).map(command => { //Accesses the non-hidden commands and returns the following
-                return {
-                    label: command.name,
-                    description: command.aliases ? command.aliases.join(', ') : null,
-                    value: command.name,
-                    emoji: command.emoji
-                };
-            });
-        const row = new MessageActionRow().addComponents(new MessageSelectMenu() //Creates a menu that has the following
-            .setCustomId(`help ${client.prefix} ${message.author.id}`)
-            .setPlaceholder('No command selected')
-            .addOptions(options)
-        );
+        const categories = new Set(new Map([...client.commands.entries()].sort()).values());
+        const categoryOptions = [...categories].map(category => {
+            return {
+                label: category,
+                value: category
+            }
+        })
+
+        const row = new MessageActionRow().addComponents(
+            new MessageSelectMenu() //Creates a menu that has all the command categories
+                .setCustomId(`help_categories ${client.prefix} ${message.author.id}`)
+                .setPlaceholder('No category selected')
+                .addOptions(categoryOptions));
 
         const embed = new MessageEmbed()
             .setColor('#DD8505')
             .setTitle(':gear: Help Menu :gear:')
             .setAuthor(message.author.username, message.author.avatarURL())
             .setThumbnail('https://cdn.discordapp.com/attachments/873564554674716765/873744094390788126/help_command_book.png')
-            .setDescription('Select a command to get started!');
+            .setDescription('Select a category to get started!');
 
         await message.reply({embeds: [embed], components: [row]});
     }
 });
+
