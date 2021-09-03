7d4f060f16c0c892c81bdf6b8f8b7c93d3c52029
divadnebnahtan <davidbennathan@gmail.com>
2021-09-03 22:54:00 +1000

    Updated alchemy commands

--- src/commands/alchemy/acraft.js
+++ src/commands/alchemy/acraft.js
@@ -1,7 +1,9 @@
-const Command = require('../../structures/command.js');
-const fs = require("fs");
-const path = require("path");
-const {MessageAttachment, MessageEmbed} = require("discord.js");
+const Command = require('../../structures/command');
+const fs = require('fs');
+const path = require('path');
+const {MessageAttachment, MessageEmbed} = require('discord.js');
+const Canvas = require("canvas");
+const GIFEncoder = require("gif-encoder-2");
 const pFile = path.resolve(__dirname, '../alchemy/players.json');
 const rFile = path.resolve(__dirname, '../alchemy/recipes.json')
 
@@ -9,12 +11,14 @@ module.exports = new Command({
     name: 'acraft',
     description: 'alchemy craft command',
     arguments: '<item>, <item>',
+    cooldown: 10,
 
     async run(message, args, client) {
+        // Read data files
+        const rData = JSON.parse(fs.readFileSync(rFile, 'utf-8'))
+        let pData = JSON.parse(fs.readFileSync(pFile, 'utf-8'))
 
-        const rData = JSON.parse(fs.readFileSync(rFile, "utf-8"))
-        let pData = JSON.parse(fs.readFileSync(pFile, "utf-8"))
-
+        // Check if user has account
         if (!pData[message.author.id]) {
             const errorEmbed = new MessageEmbed()
                 .setTitle('Error')
@@ -23,8 +27,10 @@ module.exports = new Command({
             return message.channel.send({embeds: [errorEmbed]});
         }
 
+        // Change args split to ',' (for multi word items)
         args = args.join(' ').split(',');
 
+        // Check that 2 items were given
         if (args.length < 2) {
             const errorEmbed = new MessageEmbed()
                 .setTitle('Error')
@@ -33,11 +39,14 @@ module.exports = new Command({
             return message.channel.send({embeds: [errorEmbed]});
         }
 
+        // Get item args
         const first = args[0].trim();
         const second = args[1].trim();
 
+        // Get expected product (a+b or b+a)
         let results = rData[first + '+' + second] || rData[second + '+' + first];
 
+        // Check that there is a product
         if (!results) {
             const errorEmbed = new MessageEmbed()
                 .setTitle('Error')
@@ -46,6 +55,7 @@ module.exports = new Command({
             return message.channel.send({embeds: [errorEmbed]});
         }
 
+        // Check that player has required items
         if (!(hasItem(pData[message.author.id], first) && hasItem(pData[message.author.id], second))) {
             console.log(first + ', ' + second)
             const missingItems = [first, second].filter(item => !hasItem(pData[message.author.id], item));
@@ -57,56 +67,36 @@ module.exports = new Command({
             return message.channel.send({embeds: [errorEmbed]});
         }
 
-        if (!Array.isArray(results)) results = [results];
-
-        const firstFile = new MessageAttachment(path.resolve(__dirname, '../../../assets/bigalchemy/' + first + '.png'));
-        const firstEmbed = new MessageEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL())
-            .setImage('attachment://' + first + '.png')
-
-        const secondFile = new MessageAttachment(path.resolve(__dirname, '../../../assets/bigalchemy/' + second + '.png'));
-        const secondEmbed = new MessageEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL())
-            .setImage('attachment://' + second + '.png')
-
-        if (!results.every(result => hasItem(pData[message.author.id], result))) {
-            message.channel.send({
-                embeds: [firstEmbed, secondEmbed],
-                files: [firstFile, secondFile],
-                attachments: []
-            }).then(msg => setTimeout(() => {
-                msg.edit({
-                    embeds: [secondEmbed, firstEmbed],
-                    files: [firstFile, secondFile],
-                    attachments: []
-                }).then(() => setTimeout(() => {
-                    msg.edit({
-                        embeds: [firstEmbed, secondEmbed],
-                        files: [firstFile, secondFile],
-                        attachments: []
-                    }).then(() => setTimeout(() => {
-                        sendResult(msg, results, client, message.author);
-                        saveData(message, pData, results);
-                    }, 1500));
-                }, 1500));
-            }, 1500));
-        } else {
+        // Skip the animation if
+        if (results.every(result => hasItem(pData[message.author.id], result))) {
             sendResult(message, results, client, message.author);
-            saveData(message, pData, results);
+            return saveData(message, pData, results);
         }
 
+        const itemAttachment = await craftGif(first, second);
+        const itemEmbed = new MessageEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL())
+            .setImage(`attachment://${first.replace(' ', '_')}_${second.replace(' ', '_')}.gif`);
 
+        message.channel.send({embeds: [itemEmbed], files: [itemAttachment]}).then(msg => setTimeout(() => {
+            sendResult(msg, results, client, message.author);
+            saveData(message, pData, results);
+        }, 5000));
     }
 });
 
 function saveData(message, pData, results) {
-    pData = JSON.parse(fs.readFileSync(pFile, "utf8"));
+    // Update file incase changes occurred since initial read
+    pData = JSON.parse(fs.readFileSync(pFile, 'utf8'));
+
+    // Add all new items to inventory (checks for duplicates)
     results.filter(result => !hasItem(pData[message.author.id], result)).forEach(result => pData[message.author.id].items.push(result));
 
+    // Give user 'time' item if 100 items reached
     if (pData[message.author.id].items.length >= 100 && !hasItem(pData[message.author.id], 'time')) {
         const timeFile = new MessageAttachment(path.resolve(__dirname, '../../../assets/bigalchemy/time.png'));
         const timeEmbed = new MessageEmbed()
-            .setTitle('What\'s this? A new element has been unlocked!')
+            .setTitle('What\'s this? A new element has been unlocked! You have discovered \'time\'!')
             .setAuthor(message.author.username, message.author.avatarURL())
             .setImage('attachment://time.png');
 
@@ -115,16 +105,24 @@ function saveData(message, pData, results) {
         pData[message.author.id].items.push('time');
     }
 
+    // Update file with new data
     fs.writeFileSync(pFile, JSON.stringify(pData, null, 2));
 }
 
 function sendResult(message, results, client, user) {
+
+    // Load all images
     const files = results.filter(result => result).map(result =>
-        new MessageAttachment(path.resolve(__dirname, '../../../assets/bigalchemy/' + result + '.png')));
+        new MessageAttachment(path.resolve(__dirname, '../../../assets/bigalchemy/' + result.replace(' ', '_') + '.png')));
+
+    // Create all embeds
     const embeds = results.filter(result => result).map(result => new MessageEmbed()
         .setAuthor(user.username, user.avatarURL())
-        .setImage('attachment://' + result + '.png'));
+        .setTitle(result)
+        .setImage('attachment://' + result.replace(' ', '_') + '.png'));
 
+    // Edit original message if sent by bot
+    // Otherwise just send a new message
     if (message.author.id === client.user.id) {
         message.edit({embeds: embeds, files: files, attachments: []});
     } else {
@@ -132,6 +130,38 @@ function sendResult(message, results, client, user) {
     }
 }
 
+async function craftGif(first, second) {
+    const canvas = Canvas.createCanvas(132 * 3, 132);
+    const ctx = canvas.getContext('2d');
+
+    const firstImg = await Canvas.loadImage(path.resolve(__dirname, '../../../assets/bigalchemy/' + first.replace(' ', '_') + '.png'));
+    const secondImg = await Canvas.loadImage(path.resolve(__dirname, '../../../assets/bigalchemy/' + second.replace(' ', '_') + '.png'));
+
+
+    // console.log(first)
+    // console.log(second)
+    const encoder = new GIFEncoder(132 * 3, 132);
+    encoder.setRepeat(-1);
+    encoder.setTransparent(true);
+    encoder.start();
+
+    ctx.drawImage(firstImg, 0, 0, canvas.width / 3, canvas.height);
+    ctx.drawImage(secondImg, canvas.width * 2 / 3, 0, canvas.width / 3, canvas.height);
+    encoder.addFrame(ctx);
+
+    for (let i = 0; i < canvas.width / 2 - 132 / 2; i += 5) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(firstImg, i, 0, canvas.width / 3, canvas.height);
+        ctx.drawImage(secondImg, canvas.width * 2 / 3 - i, 0, canvas.width / 3, canvas.height);
+        encoder.addFrame(ctx);
+    }
+
+    encoder.finish();
+
+    return new MessageAttachment(encoder.out.getData(), `${first.replace(' ', '_')}_${second.replace(' ', '_')}.gif`);
+}
+
 function hasItem(player, item) {
+    // Checks if the player has a specific item
     return player.items.includes(item);
 }
--- src/commands/alchemy/ainventory.js
+++ src/commands/alchemy/ainventory.js
@@ -1,28 +1,40 @@
-const Command = require('../../structures/command.js');
-const fs = require("fs");
-const path = require("path");
-const {MessageEmbed} = require("discord.js");
+const Command = require('../../structures/command');
+const fs = require('fs');
+const path = require('path');
+const {MessageEmbed} = require('discord.js');
 const pFile = path.resolve(__dirname, '../alchemy/players.json');
+const rFile = path.resolve(__dirname, '../alchemy/recipes.json')
 
 module.exports = new Command({
     name: 'ainventory',
     description: 'alchemy inventory command',
 
     async run(message) {
-        /*
-        TODO: Add total item count to inventory (123/456)
-        */
-        const pData = JSON.parse(fs.readFileSync(pFile, "utf8"));
+        // Read json files
+        const pData = JSON.parse(fs.readFileSync(pFile, 'utf-8'));
+        const rData = JSON.parse(fs.readFileSync(rFile, 'utf-8'))
 
-        if (!pData[message.author.id]) return message.reply('You need to make an account to use this command!');
+        // Return if user doesn't have an account
+        if (!pData[message.author.id]) {
+            const errorEmbed = new MessageEmbed()
+                .setTitle('Error')
+                .setDescription('You need an account to run this command.')
+                .setAuthor(message.author.username, message.author.avatarURL());
+            return message.channel.send({embeds: [errorEmbed]});
+        }
 
+        // Get every product & result from recipes.json (removes duplicates)
+        const totalItemCount = [...new Set([].concat(...Object.values(rData)).concat(...Object.keys(rData).map(key => key.split('+'))))].length;
+
+        // Get player data
         let player = pData[message.author.id];
 
+        // Send inventory embed
         const embed = new MessageEmbed()
-            .setTitle('-- ' + player.username + ' --')
+            .setTitle(`${player.username}'s inventory (${player.items.length} / ${totalItemCount})`) // Show item count
             .setColor('#DD8505')
             .setAuthor(message.author.username, message.author.avatarURL())
-            .setDescription(player.items.sort().join(', ') || 'None');
+            .setDescription(player.items.sort().join(', ') || 'None'); // Display item list
 
         message.reply({embeds: [embed]});
     }
